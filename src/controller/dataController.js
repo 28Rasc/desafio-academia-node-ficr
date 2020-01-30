cde9a4d2556db5994a34dc4b8ead65c851a16dce
Raul Cavalcanti <28rasc@gmail.com>
2020-01-30 00:26:48 +0000

    iniciado a configuração de retorno do gitAPI

--- src/controller/dataController.js
+++ src/controller/dataController.js
@@ -1,3 +1,17 @@
-//import axios from 'axios'
+import axios from 'axios'
+import { async } from 'rxjs/internal/scheduler/async';
 // deve orquestar os dados vindos do github e facebook
+const github_username = '28rasc';
+const gitApi = async function (req, res){
 
+
+if (true){
+
+            const apiResponse = await axios.get(`https://api.github.com/users/${ github_username }`);
+        
+            const { name = login, avatar_url, bio } = apiResponse.data; // buscar dados especificados na documantacao
+            
+            console.log(bio)
+}
+}
+gitApi()
