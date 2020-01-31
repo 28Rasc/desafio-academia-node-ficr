16afeb9a8990e8fc35e451af3e6855dacf32f71b
Raul Cavalcanti <28rasc@gmail.com>
2020-01-31 13:16:32 +0000

    status code return img

--- routes.js
+++ routes.js
@@ -5,7 +5,7 @@ const routes = Router()
 routes.get('/api/curriculo', getAPI)
 
 routes.get('*', function(req, res, next) {
-  res.status(404).send('Status 404');
+  res.status(404).send('<img src=https://http.cat/[status_code]>');
 });
 
 module.exports = routes
