080974213acd0203481c7b356fb9a1b6e8b4cedb
Raul Cavalcanti <28rasc@gmail.com>
2020-01-31 21:51:34 +0000

    retorno dos repositorios

--- src/controller/dataController.js
+++ src/controller/dataController.js
@@ -27,17 +27,30 @@ const getAPIs = async ( req, res) => {
         });
         
         const { name = login, html_url, bio, company, repos_url, } = gitAPI.data;
-        const repos = repoResponse.data
         const {email,birthday,gender}= fbAPI.data;
         
+         const repos = repoResponse.data.map(repo => {
+        let ReposUser = {
+          size: repo.size,
+          name: repo.name,
+          url: repo.url
+        }
+        return ReposUser;
+      });
+
+      const ListRepo = repos.splice(0, 3).sort((a, b) => {
+        if (a.size < b.size) return 1;
+        if (a.size > b.size) return -1;
+        return 0;
+      });
 
         const schema = {
             "nome": name,
             "data_nascimento": birthday,
-            "endereco": "Rua fulano de tal, 256 - Pe",
+            "endereco": "",
             "email": email,
             "genero": gender,
-            "bio": "Full Stack developer and Mobile developer",
+            "bio": "Full Stack developer",
             "foto": "https://avatars2.githubusercontent.com/u/32085246?v=4",
             "formacao": [
             {
@@ -57,26 +70,8 @@ const getAPIs = async ( req, res) => {
             },
             ],
             "github": {
-
-
             "perfil": html_url,
-            "alguns_repositorios": [
-            {
-            "size": repos[0].size,
-            "name": repos[0].name,
-            "url": repos[0].url
-            },
-            {
-            "size": repos[6].size,
-            "name":  repos[6].name,
-            "url": repos[6].url
-            },
-            {
-            "size": repos[5].size,
-            "name": repos[5].name,
-            "url": repos[5].url
-            }
-        ]
+            "alguns_repositorios": [ListRepo]
         }
             }
 
