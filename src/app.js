2b0ca8ccd98dfc4e82cc1107383786317b69c234
Raul Cavalcanti <28rasc@gmail.com>
2020-01-31 17:53:53 +0000

    config port

--- src/app.js
+++ src/app.js
@@ -1,14 +1,17 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const routes = require('./routes')
 
 const app = express()
 
+let  port = process.env.PORT || 3000
+
 app.use(express.json() );
 app.use(routes);
 app.use( cors() );
 
 
-app.listen(8080)
+app.listen(port)
 
 module.exports = app
