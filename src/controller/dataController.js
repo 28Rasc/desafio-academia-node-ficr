906756057167cbd4f62c74117baa56011ce8ddff
Raul Cavalcanti <28rasc@gmail.com>
2020-09-01 13:30:25 -0300

    Update dataController.js

--- src/controller/dataController.js
+++ src/controller/dataController.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import { async } from 'rxjs/internal/scheduler/async';
-// deve orquestar os dados vindos do github e facebook
+
 const github_username = '28rasc';
 const gitApi = async function (req, res){
 
@@ -9,7 +9,7 @@ if (true){
 
             const apiResponse = await axios.get(`https://api.github.com/users/${ github_username }`);
         
-            const { name = login, avatar_url, bio } = apiResponse.data; // buscar dados especificados na documantacao
+            const { name = login, avatar_url, bio } = apiResponse.data;
             
             console.log(bio)
 }
